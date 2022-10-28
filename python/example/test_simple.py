378cb09f1168cf8ebbe02b86df2385ebc364976d
AndreasKueck <ak163@gmx.net>
2022-10-28 23:50:45 +0200

    Update test_simple.py

--- python/example/test_simple.py
+++ python/example/test_simple.py
@@ -13,7 +13,7 @@ if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE
     print("Audio file must be WAV format mono PCM.")
     sys.exit(1)
 
-model = Model(lang="en-us")
+model = Model(lang="eo")
 
 # You can also init model by name or with a folder path
 # model = Model(model_name="vosk-model-en-us-0.21")
